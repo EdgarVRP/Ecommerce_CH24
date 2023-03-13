ef50d8c37fcfdce05e303aa650390bdfebc9f1e2
Baudelio Cuen <baudeliocf@gmail.com>
2023-03-12 17:29:56 -0700

    Contacto funcional, paleta de colores

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,5 +1,6 @@
 // alert('Contact')
 
+//Get inputs, warnings y boton
 const inputName = document.getElementById("input-name");
 const warningName = document.getElementById("warning-name");
 
@@ -17,70 +18,115 @@ const warningComments = document.getElementById("warning-comments");
 
 const buttonSend = document.getElementById("button");
 
+const form = document.getElementById("form-contact");
 
-inputName.addEventListener("input", checkName);
-inputLast.addEventListener("input", checkLast);
-inputEmail.addEventListener("input", checkEmail);
-inputPhone.addEventListener("input", checkPhone);
-inputComments.addEventListener("input", checkComments);
+
+//events: input focusout
+//focusout dispara el evento cuando se sale del formulario
+inputName.addEventListener("focusout", checkName);
+inputLast.addEventListener("focusout", checkLast);
+inputEmail.addEventListener("focusout", checkEmail);
+inputPhone.addEventListener("focusout", checkPhone);
+inputComments.addEventListener("focusout", checkComments);
 
 buttonSend.addEventListener("click", send);
 
+//Cuando se presiona el boton enviar
 function send(e) {
-    checkName(e);
-    checkLast(e);
-    checkEmail(e);
-    checkPhone(e);
-    checkComments(e);
+  //Si todo los formularios estan correctos enviar email
+  if (
+    checkName(e) ||
+    checkLast(e) ||
+    checkEmail(e) ||
+    checkPhone(e) ||
+    checkComments(e)
+  ){
+    // form.submit()
+  }
 }
 
 // Revisar que no este vacio
 function checkName(e) {
-  console.log(e);
+  // Si esta vacio muestra advertencia
   if (inputName.value == "") {
     warningName.style.visibility = "visible";
-    warningName.textContent = "No puede esta vacio";
+    warningName.textContent = "No puede estar vacio";
+
+    // Correcto
   } else {
     warningName.style.visibility = "hidden";
+    return true;
   }
+  return false;
 }
 
 // Revisar que no este vacio
 function checkLast(e) {
+  // Si esta vacio muestra advertencia
   if (inputLast.value == "") {
     warningLast.style.visibility = "visible";
-    warningLast.textContent = "No puede esta vacio";
+    warningLast.textContent = "No puede estar vacio";
+
+    // Correcto
   } else {
     warningLast.style.visibility = "hidden";
+    return true;
   }
+  return false;
 }
 
 // Revisar que no este vacio y tenga @
 function checkEmail(e) {
   if (inputEmail.value == "") {
     warningEmail.style.visibility = "visible";
-    warningEmail.textContent = "No puede esta vacio";
+    warningEmail.textContent = "No puede estar vacio";
+
+    // Si no tiene hay un @ en la cadena muestra advertencia
+  } else if (!RegExp(/@/).test(inputEmail.value)) {
+    warningEmail.style.visibility = "visible";
+    warningEmail.textContent = "Correo no valido";
+
+    // Correcto
   } else {
     warningEmail.style.visibility = "hidden";
+    return true;
   }
+  return false;
 }
 
 // Revisar que no este vacio y sean solo numeros
 function checkPhone(e) {
   if (inputPhone.value == "") {
     warningPhone.style.visibility = "visible";
-    warningPhone.textContent = "No puede esta vacio";
+    warningPhone.textContent = "No puede estar vacio";
+
+    // Si caracter no es digito en toda la cadena muestra advertencia o es diferente de 10 digitos
+  } else if (
+    !RegExp(/^\d+$/).test(inputPhone.value) ||
+    inputPhone.value.length != 10
+  ) {
+    warningPhone.style.visibility = "visible";
+    warningPhone.textContent = "Telefono no valido";
+
+    // Correcto
   } else {
     warningPhone.style.visibility = "hidden";
+    return true;
   }
+  return false;
 }
 
 // Revisar que no este vacio
 function checkComments(e) {
+  //Si no esta vacio
   if (inputComments.value == "") {
     warningComments.style.visibility = "visible";
-    warningComments.textContent = "No puede esta vacio";
+    warningComments.textContent = "No puede estar vacio";
+
+    // Correcto
   } else {
     warningComments.style.visibility = "hidden";
+    return true;
   }
+  return false;
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,31 @@
-const navbar = document.getElementById("navabar");
-navbar.innerHTML = `<nav class="navbar navbar-expand-md bg-white sticky-top border">
-    <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#pages">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+const navbar = document.getElementById("navbar");
+navbar.innerHTML = `<nav class="navbar navbar-expand-md sticky-top">
+<div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#pages">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-      <a class="navbar-brand" href="./index.html">
-        <img src="assets/img/logo.png" alt="Logo" width="36">
-        <p>Cafe Alfonso</p>
-      </a>
+  <a class="navbar-brand" href="./index.html">
+    <img src="assets/img/logo_white.png" alt="Logo" width="36">
+    <p>Cafe Alfonso</p>
+  </a>
 
-      <div class="collapse navbar-collapse" id="pages">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
-          <li class="nav-item"><a class="nav-link" href="contact.html">Contactanos</a></li>
-          <li class="nav-item"><a class="nav-link" href="about.html">Nosotros</a></li>
-          <li class="nav-item"><a class="nav-link" href="products.html">Productos</a></li>
-          <li class="nav-item"><a class="nav-link" href="signin.html">Blog</a></li>
-          <li class="nav-item"><a class="nav-link" href="login.html">Ingresar</a></li>
-        </ul>
-      </div>
-      <a class="nav-icon" href="/"><i class="bi bi-cart2"></i></a>
-    </div>
-  </nav>`;
+  <div class="collapse navbar-collapse" id="pages">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item"><a class="nav-link" href="/">Inicio</a></li>
+      <li class="nav-item"><a class="nav-link" href="contact.html">Contactanos</a></li>
+      <li class="nav-item"><a class="nav-link" href="about.html">Nosotros</a></li>
+      <li class="nav-item"><a class="nav-link" href="products.html">Productos</a></li>
+      <li class="nav-item"><a class="nav-link" href="signin.html">Blog</a></li>
+      <li class="nav-item"><a class="nav-link" href="login.html">Ingresar</a></li>
+    </ul>
+  </div>
+  <a class="nav-icon" href="/"><i class="bi bi-cart2"></i></a>
+</div>
+</nav>`;
 
 const footer = document.getElementById("footer");
-footer.innerHTML = `<footer class="footer border">
+footer.innerHTML = `<footer class="footer">
 <div class="container">
   <div class="footer-text">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, soluta.</p>
