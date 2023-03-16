68756997c4e7e365b3a94c7a23277ffaa8147b14
baudeliocf <baudeliocf@gmail.com>
2023-03-16 16:28:44 -0700

    Home video

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,33 @@
-// alert('Home')
+
+let values = "Cafe Alfonso";
+let remove = true;
+
+
+const data = [" Nataly", " Alex", " Carlos", " Santiago", " Baudelio", " Alfonso"]
+let iname = 0;
+let ichar = 0;
+
+function animation(){
+    const textVideo = document.getElementById("text-p");
+
+    if (remove){
+        values = values.substring(0, values.length - 1);
+        if (values.length < 5){
+            remove = false;
+        }
+    } else {
+        values += data[iname][ichar];
+        ichar++;
+
+        if (data[iname].length <= ichar){
+            ichar = 0;
+            iname++;
+            iname %= data.length;
+            remove = true;
+        }
+    }
+
+    textVideo.innerHTML = values;
+}
+
+setInterval(animation, 300);
