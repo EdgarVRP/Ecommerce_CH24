1768ed14178f8c45fa2971f7bad7207f394d5c99
Carlos Iturbe Gil <39778547+megath12@users.noreply.github.com>
2023-03-28 01:01:17 -0600

    Update login.js
    Actualizacion Login, pruebas

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,5 @@
 // alert('Log In')
-
+/*
 let $btn_login = document.querySelector("#btn-login");
 let $input_email = document.querySelector("#input-email");
 let $input_password = document.querySelector("#input-password");
@@ -16,3 +16,38 @@ function login() {
         window.location.href = "./";
     }
 }
+
+*/
+
+
+function miFuncion(){
+
+    const usuario = document.getElementById("email");
+    const contrasena = document.getElementById("password");
+
+    var texto="PRUUUUUUUUEEEEEBAAAA";
+    var fallo=0;
+
+    const regexpUs = /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}/
+    const regexpCo = /[a-zA-Z0-9_]\S{7,20}/
+    
+    if(!regexpUs.test(usuario)){
+        texto+="El formato del email es incorrecto";
+        fallo++;
+    }
+
+    else if(!regexpCo.test(contrasena)){
+        texto+="El tamaño de la contraseña debe ser minimo 7 caracteres";
+        fallo++;
+    }
+
+    if(fallo == 1){
+        console.log("Mal")
+    }
+    else{
+        console.log("Bien ")
+    }
+    
+    
+}
+
