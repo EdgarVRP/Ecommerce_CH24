82c5ddd5cc32488a6cf85ddb9d72efe3d73d9862
AlexDev <snzlx@outlook.com>
2023-04-03 01:10:34 -0600

    limpieza del codigo

--- scripts/products.js
+++ scripts/products.js
@@ -72,9 +72,7 @@ function showProducts() {
 	}
 }
 
-/* 
-<button type="button" class="btn btn-primary flex-fill me-1 btnVerMas" data-mdb-ripple-color="dark" data-bs-toggle="modal" data-bs-target="#modal-product" style="width: 150px;" onclick"updateModal()">Detalles</button>
- */
+
 
 function updateModal(i) {
 	//document.getElementById("modal-img").src = PRODUCT.rutaImagen;
@@ -151,64 +149,6 @@ function updateModal(i) {
     
     modalDivBody.innerHTML += modalDivBodyInfo;
     
-    //console.log("hola");
-	//document.getElementById("modal-count").innerHTML = DATA_CART[i].count;
-	// alert(id)
+   
 }
 
-
-
-
-  
-/* <button type="button" class="btn btn-primary flex-fill me-1 btnVerMas" data-mdb-ripple-color="dark" data-toggle="modal" data-target="#products-container" onclick="modalVerMas">
-                      Ver más
-                      </button> */
-
-/*
-`<div class="col-md-12 col-lg-4 mb-4 mb-lg-0">
-          <div class="card productCard">
-              <img src="${products[i].rutaImagen}"
-              class="card-img-top" alt="ImagenVariedadPacamara" />
-              <div class="card-body">
-                  <div class="text-center mt-1">
-                      <h4 class="card-title">${products[i].region}</h4>
-                      <h6 class="h6 mb-1 pb-2 lightYellowText">Desde: $${products[i].precioTostado}</h6>
-                  </div>
-      
-                  <div class="text-center">
-                      <div class="p-3 mx-n3 mb-2" style="background-color: #2e151a; border-radius: 15px;">
-                          <h5 class="h4 mb-0 lightYellowText">Vistazo</h5>
-                      </div>
-      
-                      <div class="d-flex flex-column mb-3">
-                          <span class="h5 mb-0 lightYellowText">Región: </span>
-                          <span class="h6 mb-0 lightYellowText">Corzo Zona Frailesca, Chiapas</span>
-                      </div>
-                      
-                      <div class="d-flex flex-column mb-3">
-                          <span class="h5 mb-0 lightYellowText">Altura: </span>
-                          <span class="h6 mb-0 lightYellowText">1300 - 1500 MSNM</span>
-                      </div>
-      
-                      <div class="d-flex flex-column mb-3">
-                          <span class="h5 mb-0 lightYellowText">Humedad: </span>
-                          <span class="h6 mb-0 lightYellowText">11 %</span>
-                      </div>
-                                                  
-                      <div class="p-3 mx-n3 mb-4" style="background-color: #2e151a; border-radius: 15px;">
-                          <h5 class="h5 mb-0 lightYellowText">Notas: </h5>
-                          <span class="h6 mb-0 lightYellowText">${products[i].nota}</span>
-                      </div>
-                      
-                  </div>
-      
-                  <div class="d-flex flex-row">
-                      <button type="button" class="btn btn-primary flex-fill me-1 btnVerMas" data-mdb-ripple-color="dark">
-                      Ver más
-                      </button>
-                      <button type="button" class="btn btn-danger flex-fill ms-1 btnBuyNow">Comprar ahora</button>
-                  </div>
-              </div>
-          </div>
-          </div>`;
-*/
