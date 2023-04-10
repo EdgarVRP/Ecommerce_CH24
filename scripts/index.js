5d8450e869b3a7039bc2f042cbe06d59e84dcd1e
AlexDev <snzlx@outlook.com>
2023-04-09 22:08:50 -0600

    Mensaje de bienvenida corregido

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,22 @@
-let values = "Bienvenidos";
-let remove = true;
+let values = "";
+let remove = false;
 
-
-const data = [" querido", " consumidor"]
+//const data = ["Bienvenid", "os queridos", "os consumidores", "as queridas", "as consumidoras", "es querides ", "es consumidores"];
+const data = ["Bienvenidos ", " queridos", " consumidores"];
 let iname = 0;
 let ichar = 0;
 
 function animation(){
     const textVideo = document.getElementById("text-p");
-
+    
     if (remove){
         values = values.substring(0, values.length-1);
         if (values.length < 12){
             remove = false;
+            //values = "Bienvenid";
+            values = "Bienvenidos ";
         }
+
     } else {
         values += data[iname][ichar];
         ichar++;
@@ -22,11 +25,13 @@ function animation(){
             ichar = 0;
             iname++;
             iname %= data.length;
+            if (iname == 0) {iname = 1;}
             remove = true;
+            //values = "Bienvenidos";
         }
     }
 
     textVideo.innerHTML = values;
 }
 
-setInterval(animation, 300);
+setInterval(animation, 200);
